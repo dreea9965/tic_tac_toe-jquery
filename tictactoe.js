e110778c7e5e38a9b8ff0251c839d5d0685bc059
Andreea Uta <dreea.9965@gmail.com>
2017-03-08 16:03:11 -0500

    more tic tac toe

--- tictactoe.js
+++ tictactoe.js
@@ -1,101 +1,125 @@
+// <script>
+// $('document').ready(function() {
+//   var j = 1;
+//
+//   $('.button').click(function() {
+//
+//     if (j === 1){
+//       $(this).text('x');
+//       j += 1;
+//     }
+//     else if ( j === 2){
+//       // $(this).toggleClass('x');
+//       $(this).text('o');
+//       j += 1;
+//     }
+//
+//     else {
+//       // $(this).toggleClass('o');
+//       j = 1;
+//     }
+//
+//     });
+//
+//
+// });
 
-var win_array = [
+// 
+// var win_array = [
+//
+//   //horizontal
+//   [$('.1').hasClass(), $('.2').hasClass(), $('.3').hasClass()],
+//   [$('.4').hasClass(), $('.5').hasClass(), $('.6').hasClass()],
+//   [$('.7').hasClass(), $('.8').hasClass(), $('.9').hasClass()],
+//
+//   //vertical
+//   [$('.1').hasClass(), $('.4').hasClass(), $('.7').hasClass()],
+//   [$('.2').hasClass(), $('.5').hasClass(), $('.8').hasClass()],
+//   [$('.3').hasClass(), $('.6').hasClass(), $('.9').hasClass()],
+//
+//   [$('.1').hasClass(), $('.5').hasClass(), $('.9').hasClass()],
+//   [$('.3').hasClass(), $('.5').hasClass(), $('.7').hasClass()]
+// ];
 
-  //horizontal
-  [$('#1').text(), $('#2').text(), $('#3').text()],
-  [$('#4').text(), $('#5').text(), $('#6').text()],
-  [$('#7').text(), $('#8').text(), $('#9').text()],
-
-  //vertical
-  [$('#1').text(), $('#4').text(), $('#7').text()],
-  [$('#2').text(), $('#5').text(), $('#8').text()],
-  [$('#3').text(), $('#6').text(), $('#9').text()],
-
-  [$('#1').text(), $('#5').text(), $('#9').text()],
-  [$('#3').text(), $('#5').text(), $('#7').text()]
-];
+$('document').ready(function() {
+   var player = 1;
 
 
-function playerWon(winner) {
+  $('.button').click(function(){
+    var selected = $(this);
+    //var is this button
 
-  if ($('#1').text() && $('#2').text() && $('#3').text()){
+  if (selected.hasClass('x') || selected.hasClass('o')){
+      //if x or o has already been selected, do nothing.
+    }
+
+  else {
+      if (player === 1 ) {
+        selected.addClass('x');
+        player = 2;
+        //make it play 2's turn
+        if (checkWon('x')) {
+          //
+          alert('Player 1 has Won!');
+        }
+       }
+      else {
+        selected.addClass('o');
+        player = 1;
+        //switch turns to player 1
+        if (checkWon('o')) {
+          alert('Player 2 has Won!'); }
+    }
+  }
+});
+
+function checkWon(winner) {
+
+// if 1 + 2 + 3 return true. player wins!
+  if ($('.1').hasClass(winner) && $('.2').hasClass(winner) && $('.3').hasClass(winner)){
     return true;
   }
-  else if ($('#4').text() && $('#5').text() && $('#6').text()){
+  else if ($('.4').hasClass(winner) && $('.5').hasClass(winner) && $('.6').hasClass(winner)){
     return true;
   }
 
-  else if ($('#7').text() && $('#8').text() && $('#9').text()){
+  else if ($('.7').hasClass(winner) && $('.8').hasClass(winner) && $('.9').hasClass(winner)){
     return true;
   }
 
-  else if ($('#1').text() && $('#4').text() && $('#7').text()){
+  else if ($('.1').hasClass(winner) && $('.4').hasClass(winner) && $('.7').hasClass(winner)){
     return true;
   }
 
-  else if ($('#2').text() && $('#5').text() && $('#8').text()){
+  else if ($('.2').hasClass(winner) && $('.5').hasClass(winner) && $('.8').hasClass(winner)){
     return true;
   }
 
-  else if ($('#3').text() && $('#6').text() && $('#9').text()){
+  else if ($('.3').hasClass(winner) && $('.6').hasClass(winner) && $('.9').hasClass(winner)){
     return true;
   }
-  else if ($('#1').text() && $('#5').text() && $('#9').text()){
+  else if ($('.1').hasClass(winner) && $('.5').hasClass(winner) && $('.9').hasClass(winner)){
     return true;
   }
 
-  else if ($('#3').text() && $('#5').text() && $('#7').text()){
+  else if ($('.3').hasClass(winner) && $('.5').hasClass(winner) && $('.7').hasClass(winner)){
     return true;
   }
   else {
     return false;
+    // keep playing
   }
 
 }
 
-if (playerWon === true) {
-  alert('player has won!');
-}
 
-// var one = $('#1').text();
-// var two = $('#2').text();
-// var three = $('#3').text();
-// var four = $('#4').text();
-// var five = $('#5').text();
-// var six = $('#6').text();
-// var seven = $('#7').text();
-// var eight = $('#8').text();
-// var nine = $('#9').text();
+$('.resetButton').click(function() {
+  $('.o').removeAtr(".button");
+  $('.x').removeAtr(".button");
+
+   });
 
 
 
 
-// $('tr').map(function()
-//  { return $(this).text();
-//  });
-
-
-// var Turn = "X";
-// $("#turns").html(Turn);
-//
-// $(".tile").click(function(){
-//
-//   $(this).html(Turn);
-//   if(Turn === "X"){
-//     Turn = "O";
-//   }
-//   else {
-//     Turn = "X";
-//    }
-//
-//
-//
-//   }
-//
-// $("#turns").html(Turn)
-//  });
-//
-//
-// $("tr, td").click(function(){
-//   $(".tile").html("");
-// });
+});
